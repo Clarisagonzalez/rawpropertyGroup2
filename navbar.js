d96f39e5c3fc35bab388b096da986c7955b58eea
Clarisagonzalez <136874433+Clarisagonzalez@users.noreply.github.com>
2024-05-23 13:20:50 -0400

    navbar

--- navbar.js
+++ navbar.js
@@ -1,5 +1,14 @@
-// navbar.js
-$(function() {
-    $("#navbar-placeholder").load("navbar.html");
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('navbar.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to fetch navbar');
+        }
+        return response.text();
+    })
+    .then(data => {
+        console.log('Navbar loaded successfully');
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    })
+    .catch(error => console.error('Error loading navbar:', error));
 });
-
